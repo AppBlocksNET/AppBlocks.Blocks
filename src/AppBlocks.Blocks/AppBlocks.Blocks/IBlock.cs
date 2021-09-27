0119a055228f8da81281f0d45c6cc924489705d1
David Walker <dave@radicaldave.com>
2021-09-27 16:28:55 -0500

    init

---
+++ src/AppBlocks.Blocks/AppBlocks.Blocks/IBlock.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AppBlocks.Blocks
+{
+    public interface IBlock<T>
+    {
+        Task<IEnumerable<T>> ExecuteAsync(Dictionary<string, string> settings = null);
+    }
+}
